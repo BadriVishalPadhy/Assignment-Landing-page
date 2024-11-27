5421842c61e4748e52a21ab45a2862a09688b481
Badri-Vishal <badrivishalpadhy@gmail.com>
2024-11-27 18:08:25 +0530

    fixes/comparison-section

--- src/components/comparison-section.tsx
+++ src/components/comparison-section.tsx
@@ -10,17 +10,17 @@ const comparisonData = [
 
 export function ComparisonSection() {
   return (
-    <section className="py-16 md:py-24 from-[#0A1F1C] to-[#0A0B1F] relative overflow-hidden bg-[url('/image2.png')]">
+    <section className=" flex items-center justify-center py-16 md:py-24 from-[#0A1F1C] to-[#0A0B1F] relative overflow-hidden bg-[url('/image2.png')]">
       <div className="container px-4">
         <h2 className="text-5xl font-machina  font-bold mb-12">
           Why <span className="text-yellow-500">MoonEX</span> ?
         </h2>
         
-        <div className="relative overflow-x-auto">
+        <div className="relative overflow-x-auto bg-[#061724]/80 rounded-3xl p-8 md:p-12 backdrop-blur-sm border border-white/10 shadow-2xl">
           <table className="w-full">
             <thead>
               <tr className="border-b border-gray-800">
-                <th className="text-left text-xl md:text-3xl font-fira py-4 text-yellow-500 w-1/3">Comparison</th>
+                <th className="text-left font-machina text-xl md:text-3xl  py-4 text-yellow-500 w-1/3">Comparison</th>
                 <th className="text-center py-4 w-1/3 relative">
                   <div className="flex items-center justify-center">
                     <img src="/moon-icon.png" alt="MoonEX Logo" className="w-11 h-11 hidden md:block" />
